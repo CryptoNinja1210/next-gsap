412b8599685b297fc23ca185083997eca634345e
SweetCoding115 <moriahbuckridge96115@gmail.com>
2023-07-28 07:42:43 +0800

    fixed some parts

--- components/Begin/Begin.js
+++ components/Begin/Begin.js
@@ -13,6 +13,7 @@ const Begin = ({ isDesktop, clientHeight }) => {
 	const leftsideText = useRef(null);
 	const rightsideText = useRef(null);
 	const middlesideText = useRef(null);
+	const middleExitText = useRef(null);
 
 	useEffect(() => {
 		const [revealTimeline, revealScrollTrigger] = getRevealSt();
@@ -197,7 +198,18 @@ const Begin = ({ isDesktop, clientHeight }) => {
 					duration: 2,
 				},
 				"<"
-			);
+			)
+			.fromTo(
+				middleExitText.current,
+				{ fontSize: 20, opacity: 0, skewY: 0, rotateY: 0, },
+				{ fontSize: 60, opacity: 1, skewY: -30, rotateY: -30, duration: 0.5, },
+				"<"
+			)
+			.to(
+				middleExitText.current,
+				{ translateY: -window.innerHeight, duration: 1, }
+			)
+			;
 		const scrollTrigger = ScrollTrigger.create({
 			trigger: wrapperRef.current,
 			start: "bottom bottom",
@@ -225,8 +237,11 @@ const Begin = ({ isDesktop, clientHeight }) => {
 					Let&apos;s begin
 				</span>
 			</div>
-			<div ref={middlesideText} className="z-50 flex justify-center items-center align-middle mb-[150px] p-0 left-[-50px]  whitespace-nowrap text-primary-purple">
+			<div ref={middlesideText} className="z-50 flex justify-center items-center align-middle mb-[150px] p-0 left-[-50px] whitespace-nowrap text-primary-purple">
 				<span>together</span>
+			</div>
+			<div ref={middleExitText} className="absolute w-full h-full flex z-50 justify-center items-center  whitespace-nowrap text-primary-purple text-6xl">
+				<span>Public exit experience</span>
 			</div>
 			<div ref={rightPhone} id="right-phone" className=" flex flex-col text-left gap-5">
 				<span ref={toprightText} className="pl-5 text-primary-purple whitespace-nowrap">
